523aefa5c607d2562bb1cd85e74d16af64900072
Vishnu Rathore <81683825+Rkvishnu@users.noreply.github.com>
2023-09-17 19:32:29 +0530

    Update admin-auth.guard.ts

--- weather-bot/src/admin-panel/admin-auth.guard.ts
+++ weather-bot/src/admin-panel/admin-auth.guard.ts
@@ -8,13 +8,11 @@ export class AdminAuthGuard implements CanActivate {
   constructor(private readonly reflector: Reflector) {}
 
   canActivate(context: ExecutionContext): boolean {
-    // Implement logic to check if the user has admin privileges
-    // You can access user information from context.switchToHttp().getRequest()
-    // Example:
-    // const user = context.switchToHttp().getRequest().user;
-    // if (user && user.isAdmin) {
-    //   return true;
-    // }
+    
+    const user = context.switchToHttp().getRequest().user;
+    if (user && user.isAdmin) {
+      return true;
+    }
     return false;
   }
 }
